b7087950e7f46cc46c75416eca24eabf3f06406c
Hugo Quadros <83436359+HugoQuadros@users.noreply.github.com>
2021-08-30 12:08:19 -0300

    criando o model contato

--- src/app/components/lista-de-contatos/lista-de-contatos.component.ts
+++ src/app/components/lista-de-contatos/lista-de-contatos.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
+import { Contato } from 'src/app/models/contato';
 
 @Component({
   selector: 'app-lista-de-contatos',
@@ -6,6 +8,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lista-de-contatos.component.css']
 })
 export class ListaDeContatosComponent implements OnInit {
+
+  contatos:Contato[] = [
+    {
+      nome: "Zé das Couve",
+      email: "zedascouve@teste.com",
+      telefones: ["99999-9999"]
+    },
+    {
+      nome: "Zé das Couve 2",
+      email: "zedascouve@teste.com",
+      telefones: ["98888-8888"]
+    },
+    {
+      nome: "Zé das Couve 3",
+      email: "zedascouve@teste.com",
+      telefones: ["97777-7777"]
+    },
+    {
+      nome: "Zé das Couve 4",
+      email: "zedascouve@teste.com",
+      telefones: ["96666-6666"]
+    },
+    {
+      nome: "Zé das Couve 5",
+      email: "zedascouve@teste.com",
+      telefones: ["95555-5555"]
+    },
+    {
+      nome: "Zé das Couve 6",
+      email: "zedascouve@teste.com",
+      telefones: ["94444-4444"]
+    },
+    {
+      nome: "Zé das Couve 7",
+      email: "zedascouve@teste.com",
+      telefones: ["93333-3333"]
+    },
+    {
+      nome: "Zé das Couve 8",
+      email: "zedascouve@teste.com",
+      telefones: ["92222-2222"]
+    }
+  ]
 
   constructor() { }
 
