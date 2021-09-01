eeaf2eac22010265bbfefa0b1a492101d6b2c83c
Hugo Quadros <83436359+HugoQuadros@users.noreply.github.com>
2021-09-01 16:30:00 -0300

    campos model interativos

--- src/app/components/create-contato/create-contato.component.ts
+++ src/app/components/create-contato/create-contato.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Contato } from 'src/app/models/contato';
 
 @Component({
   selector: 'app-create-contato',
@@ -9,11 +10,21 @@ export class CreateContatoComponent implements OnInit {
 
   @Output() onCancelarClick = new EventEmitter();
 
+  novoContato:Contato = {
+    nome:"",
+    email:"",
+    telefones:["7777-7777", "8888-8888", "9999-9999"]
+  }
+
   hide():void {
     this.onCancelarClick.emit();
   }
   
   constructor() { }
+
+  track(index:number, value:string){
+    return index;
+  }
 
   ngOnInit(): void {
   }
